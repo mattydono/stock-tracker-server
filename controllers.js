e6f5a6513ebe24f2923a766f647cdf677e2993ed
Egill Hreinson <egillh210@gmail.com>
2019-09-26 10:36:55 +0200

    feat: quote, news, and peers routes

--- controllers.js
+++ controllers.js
@@ -1,10 +1,40 @@
-import { company } from 'iexcloud_api_wrapper';
+import { company, quote, news, peers } from 'iexcloud_api_wrapper';
 
 export const companyRequest = async (req, res) => {
     try {
         const { ticker } = req.params;
         const result = await company(ticker);
         res.status(200).send(result)
+    } catch (e) {
+        res.status(500).send(e);
+    }
+}
+
+export const quoteRequest = async (req, res) => {
+    try {
+        const { ticker } = req.params;
+        const result = await quote(ticker);
+        res.status(200).send(result);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+}
+
+export const peersRequest = async (req, res) => {
+    try {
+        const { ticker } = req.params;
+        const result = await peers(ticker);
+        res.status(200).send(result);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+}
+
+export const newsRequest = async (req, res) => {
+    try {
+        const { ticker } = req.params;
+        const result = await news(ticker);
+        res.status(200).send(result);
     } catch (e) {
         res.status(500).send(e);
     }
